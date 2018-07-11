cb722bf564c206a5b53ab6aa257c20549eec5729
Kyle Anderson <kyle-l-anderson@uiowa.edu>
2018-07-11 00:09:41 -0500

    install script

---
+++ setup.ps1
@@ -0,0 +1,11 @@
+choco install chocolatey 
+choco install adobereader flashplayerplugin googlechrome firefox
+choco install chocolatey-core.extension
+choco install jre8 javaruntime jdk8
+choco install git.install git 
+choco install putty.install putty
+choco install nodejs.install nodejs
+choco install notepadplusplus.install notepadplusplus
+choco install openssh curl wget
+choco install ruby python3
+choco install virtualbox wireshark cygwin github awscli
